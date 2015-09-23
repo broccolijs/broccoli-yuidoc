5ca86976a6f5617676a06664730f39c763b4df44
Marco Otte-Witte <marco.otte-witte@simplabs.com>
2015-09-23 18:11:57 +0200

    WIP: adapt to latest caching writer api

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 'use strict';
 
-var merge   = require('merge');
-var yuidoc  = require('yuidocjs');
-var rsvp    = require('rsvp');
-var fs      = require('fs');
-var path    = require('path');
+var merge         = require('merge');
+var yuidoc        = require('yuidocjs');
+var rsvp          = require('rsvp');
+var fs            = require('fs');
+var path          = require('path');
 var CachingWriter = require('broccoli-caching-writer');
 
 var defaultOpts = {
-  srcDir: 'app',
   destDir: 'docs',
   yuidoc: {
     linkNatives: true,
@@ -18,18 +17,29 @@ var defaultOpts = {
   }
 };
 
+var BroccoliYuidoc = function BroccoliYuidoc(inputNodes, options) {
+  if (!(this instanceof BroccoliYuidoc)) {
+    return new BroccoliYuidoc(inputNodes, options);
+  }
 
-var BroccoliYuidoc = CachingWriter.extend({
-  init: function(inputTree) {
-    this.inputTree = inputTree;
-    this.yuidoc = merge(defaultOpts, this.yuidoc);
-  },
+  options = options || {};
+  CachingWriter.call(this, inputNodes, {
+    annotation: options.annotation
+  });
+  this.inputNodes = inputNodes;
+  this.options    = merge(defaultOpts, options);
+};
 
-  updateCache: function(srcPaths, destDir) {
-    var options = this.yuidoc;
+BroccoliYuidoc.prototype = Object.create(CachingWriter.prototype);
+BroccoliYuidoc.prototype.constructor = BroccoliYuidoc;
+BroccoliYuidoc.prototype.description = 'yuidoc';
+BroccoliYuidoc.prototype.write = function(readTree, destDir) {
+  var self = this;
+  return readTree(this.inputNodes).then(function() {
+    var options = self.options;
 
     options.paths = options.paths || srcPaths;
-    options.outdir = [destDir, this.destDir].join('/');
+    options.outdir = [destDir, self.destDir].join('/');
 
      try {
        var json = (new yuidoc.YUIDoc(options)).run();
@@ -45,12 +55,11 @@ var BroccoliYuidoc = CachingWriter.extend({
     }
 
     var builder = new yuidoc.DocBuilder(options, json);
-    var self = this;
 
     return new rsvp.Promise(function(resolve) {
       builder.compile(function() { resolve(); });
     });
-  }
-});
+  });
+};
 
 module.exports = BroccoliYuidoc;
